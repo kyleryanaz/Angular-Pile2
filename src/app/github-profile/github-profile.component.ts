5d8e528df32d2708fc11b864cca7d3b4f24bce25
Kyle <kyleryan480@gmail.com>
2018-09-15 18:01:25 -0700

    Navigating/passing params with functions

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-github-profile",
@@ -7,19 +8,27 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["./github-profile.component.css"]
 })
 export class GithubProfileComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    // private route: ActivatedRoute
+    private router: Router
+  ) {}
+
+  submit() {
+    this.router.navigate(["/followers"], {
+      queryParams: { page: 1, order: "newest" }
+    });
+  }
 
   ngOnInit() {
     // Takes a snapshot of the route params. This is good if you are sure that the view does not
     // need to update anything after it is initialized.
     // let id = this.route.snapshot.paramMap.get('id');
     // console.log(id);
-
     // Subscribing to an observable allows Angular to keep one instance of this component up and
     // continually update its data. Without this, the component would be destroyed each time it
     // was navigated away from and re-initialized when navigated back to.
-    this.route.paramMap.subscribe(params => {
-      console.log(params.get("id"));
-    });
+    // this.route.paramMap.subscribe(params => {
+    //   console.log(params.get("id"));
+    // });
   }
 }
